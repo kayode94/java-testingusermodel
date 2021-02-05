4158e5ff75660dbb9ac8d0b1a0a8eeaef0e1c99d
kayode <kayode.richards@gmail.com>
2021-02-05 15:14:47 -0500

    complete

---
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/usermodel/controllers/UserControllerTest.java
@@ -0,0 +1,68 @@
+package com.lambdaschool.usermodel.usermodel.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserControllerTest
+{
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void listAllUsers()
+    {
+
+    }
+
+    @Test
+    public void getUserById()
+    {
+
+    }
+
+    @Test
+    public void getUserByName()
+    {
+
+    }
+
+    @Test
+    public void getUserLikeName()
+    {
+
+    }
+
+    @Test
+    public void addNewUser()
+    {
+
+    }
+
+    @Test
+    public void updateFullUser()
+    {
+
+    }
+
+    @Test
+    public void updateUser()
+    {
+
+    }
+
+    @Test
+    public void deleteUserById()
+    {
+        
+    }
+}
